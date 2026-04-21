ac02df46c25724a136d4febea98d0cead9708eb8
zuse-z3 <knowledg3@posteo.de>
2026-04-21 12:32:42 +0200

    feat: parse SCS flavor

--- internal/collector/vminfo.go
+++ internal/collector/vminfo.go
@@ -3,6 +3,8 @@ package collector
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/eco-digit/leaf/internal/infrastructure"
 	prommodel "github.com/prometheus/common/model"
@@ -12,22 +14,23 @@ type VMInfo struct {
 	VMID        string
 	ProjectID   string
 	ProjectName string
-	FlavourName string
-	VCPUs       float64
-	MemoryGB    float64
+	FlavorName  string
+	VCPUs       int
+	MemoryGB    int
 }
 
 // collectVMInfo extracts VM information per Project
-func collectVMInfo(q Querier, src *infrastructure.VMInfoSourceDef, r RawMetrics) {
+func collectVMInfo(q Querier, src *infrastructure.VMInfoSourceDef, r *RawMetrics) {
 	if src == nil {
 		return
 	}
 
 	val, err := q.QueryMetric(src.Metric)
 	if err != nil {
-		warn := fmt.Sprintf("s% query failed: %v", src.Metric, err)
+		warn := fmt.Sprintf("%s query failed: %v", src.Metric, err)
 		log.Printf("collector warning: %s", warn)
 		r.Warnings = append(r.Warnings, warn)
+		return
 	}
 
 	vec, _ := val.(prommodel.Vector)
@@ -37,20 +40,42 @@ func collectVMInfo(q Querier, src *infrastructure.VMInfoSourceDef, r RawMetrics)
 			continue
 		}
 		flavor := string(s.Metric[prommodel.LabelName(src.FlavorLabel)])
-		// TODO parse Flavor
-		//  vcpus, memGB:= ParseSCSFlavor()
+		vcpus, memGB := ParseSCSFlavor(flavor)
 		r.VMInfos = append(r.VMInfos, VMInfo{
 			VMID:        uuid,
 			ProjectID:   string(s.Metric[prommodel.LabelName(src.ProjectIDLabel)]),
 			ProjectName: string(s.Metric[prommodel.LabelName(src.ProjectNameLabel)]),
-			FlavourName: flavor,
-			// vcpus
+			FlavorName:  flavor,
+			VCPUs:       vcpus,
+			MemoryGB:    memGB,
 			// vmem
 		})
 
 	}
 }
 
-func ParseSCSFlavor(name string) (vcpus float64, ramGB float64) {
-	return
+// ParseSCSFlavor
+// WIP in general it makes sense that leaf knows the difference between different
+// SCS CPU suffixes or disk sizes and types
+func ParseSCSFlavor(name string) (vcpus int, memGB int) {
+	if !strings.HasPrefix(name, "SCS-") {
+		return 0, 0
+	}
+	parts := strings.Split(name[4:], "-")
+	if len(parts) < 2 {
+		return 0, 0
+	}
+	return parseLeadingInt(parts[0]), parseLeadingInt(parts[1])
+}
+
+func parseLeadingInt(s string) int {
+	i := 0
+	for i < len(s) && s[i] >= '0' && s[i] <= '9' {
+	}
+	i++
+	if i == 0 {
+		return 0
+	}
+	v, _ := strconv.Atoi(s[:i])
+	return v
 }
